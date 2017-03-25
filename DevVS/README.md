8e488aa2706adb514de25864c091842c0b24d735
chip.willman@gmail.com <chip.willman@gmail.com>
2017-03-24 18:18:04 -0700

    Added Dev VS

---
+++ DevVS/README.md
@@ -0,0 +1,8 @@
+# Dev Visual Studio
+
+Setup a Visual Studio development environment
+
+```
+.\setup_devenv.ps1
+
+```
---
+++ DevVS/setup_devenv.ps1
@@ -0,0 +1,8 @@
+iwr https://chocolatey.org/install.ps1 | iex
+
+cinst -y visualstudio2015professional
+cinst -y git
+cinst -y googlechrome
+cinst -y totalcommander
+cinst -y notepadplusplus
+cinst -y dotnet4.6
